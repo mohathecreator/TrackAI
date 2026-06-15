74a266b08fb5e2d1a82c3e5b7f60551652c6d77b
Mohamed El Domyaty <mohaeldomyatty@gmail.com>
2026-06-15 16:34:46 +0200

    feat: define forward function for CNN

--- model.py
+++ model.py
@@ -15,4 +15,20 @@ class TrackmaniaNet(nn.Module):
 
         self.dropout = nn.Dropout(0.5)
 
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
 
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+
+        x = F.relu(self.conv3(x))
+        x = F.max_pool2d(x, 2)
+
+        x = x.view(x.size(0), -1)
+
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = torch.sigmoid(self.fc2(x))
+
+        return x
